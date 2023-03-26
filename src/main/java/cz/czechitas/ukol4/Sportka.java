56f1d33f69c0983d154a6dc40100b0958b8a368f
Vendula Stravová <venom111@email.cz>
2023-03-26 18:28:07 +0200

    list naplněn 1-49

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -12,6 +12,12 @@ public class Sportka {
 
   public Sportka() {
     //TODO naplnit osudí čísly 1 až 49.
+
+    private void naplnOsudi() {
+      for (int i = 1; i < 49; i++) {
+        osudi.add(i);
+      }
+          }
   }
 
   /**
@@ -21,6 +27,9 @@ public class Sportka {
    */
   public void zamichat() {
     //TODO zamíchat osudí
+
+Collections.shuffle(osudi);
+
   }
 
   /**
